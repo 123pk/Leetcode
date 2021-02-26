8685898aa2c0f94fc98a5232142096489b25a81f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-26 22:40:26 +0530

    Create Validate Stack Sequences.cpp

---
+++ Validate Stack Sequences.cpp
@@ -0,0 +1,19 @@
+/*
+Platform :- Leetcode
+Problem :- Valid Stack Sequence
+*/
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>P;
+        int i=0,j=0;
+        for(i=0;i<pushed.size();++i){
+            P.push(pushed[i]);
+            while(!P.empty() && P.top()==popped[j]){
+                P.pop();
+                j++;
+            }
+        }
+        return P.empty();
+    }
+};
