55b29d9f1ce390546be4c9457ee50e66293d833d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-22 22:11:03 +0530

    Update Pascal Triangle.cpp

--- Pascal Triangle.cpp
+++ Pascal Triangle.cpp
@@ -9,3 +9,25 @@ Approach :- we can analyse that no of elementh at 'ith' level is 'i' ,
                  A[i][j] = A[i-1][j-1] + A[i-1][j]
 */
 
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<n;++i){
+            vector<int>temp(i+1);
+            if(i==0){
+                temp[i]=1;
+            }
+            else {
+                temp[0]=1;
+                temp[i]=1;
+                for(int j=1;j<i;++j){
+                    temp[j]=ans[i-1][j-1]+ans[i-1][j];
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
