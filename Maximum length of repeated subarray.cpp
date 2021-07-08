29f9f2e70281c5c5f2b5812b938619435bec3704
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-08 21:12:57 +0530

    Update Maximum length of repeated subarray.cpp

--- Maximum length of repeated subarray.cpp
+++ Maximum length of repeated subarray.cpp
@@ -6,3 +6,30 @@ Approach :- draw a 2-d matrix of pair where A[i][j] have a pair of { nums1[i] , 
             iii. if second condition is not met we store dp[i][j] = 0
             iv. after each operation we store the max value in ans and return at end
 */
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int ans=0;
+        int n=nums1.size();
+        int m=nums2.size();
+        
+        int dp[n][m];
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(i==0||j==0){
+                    if(nums1[i]==nums2[j])dp[i][j]=1;
+                    else dp[i][j]=0;
+                }
+                else{
+                    if(nums1[i]==nums2[j])dp[i][j]=1+dp[i-1][j-1];
+                    else dp[i][j]=0;
+                    
+                }
+                 ans=max(ans,dp[i][j]);
+            }
+        }
+        
+        
+        return ans;
+    }
+};
