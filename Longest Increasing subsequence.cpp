c0f2f545246750908ced22d53164f298bf603277
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-09 21:22:51 +0530

    Update Longest Increasing subsequence.cpp

--- Longest Increasing subsequence.cpp
+++ Longest Increasing subsequence.cpp
@@ -6,3 +6,26 @@ Approach :- for each index i from [ 0 to nums.size() ] find the max subsequence 
             index is ----> [ max( val[i] , val[j]+1) ] where val[i] = max subsequence length till ith index
             then out of all values for each index return the max one .
 */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& A) {
+        int n=A.size();
+        
+        //val store the max length of subsequence till for ith index
+        vector<int>val(n,1);
+        for(int i=1;i<n;++i){
+                for(int j=0;j<i;++j){
+                    if(A[j]<A[i]){
+                        val[i]=max(val[i],val[j]+1);
+                    }
+                }
+        }
+        
+        
+        int ans=INT_MIN;
+        for(auto &x:val){
+            ans=max(ans,x);
+        }
+        return ans;
+    }
+};
