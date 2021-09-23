11903e35d21ddcfb5bbe76b2232bdbcdaa52472c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-23 22:50:02 +0530

    Update Break a palinndrome.cpp

--- Break a palinndrome.cpp
+++ Break a palinndrome.cpp
@@ -1,1 +1,8 @@
-
+/*
+Platform :- Github
+Approach :- Take care of case when:-
+          i. length of string in 1 ,here we cannot make a string which is not a palindrome
+          ii. length is 2 or 3 here if first letter is 'a' then we change the last letter to'b' else we change the first letter to 'a'.
+          iii. for all other lengths we traverse through the first half and loof for values which are not 'a' if we found any we change it to 'a' else
+               we change the last letter to 'b'.
+*/
