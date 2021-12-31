68045582cafa72d87aad2e11be51e5b6f43f14ae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-12-31 21:49:37 +0530

    Create Maximum difference between node and ancestor.cpp

---
+++ Maximum difference between node and ancestor.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- BFS and maintain a min and max , whenever visiting any node find the absolute difference between node val and min and max and store in answer and update min and max .
+*/
